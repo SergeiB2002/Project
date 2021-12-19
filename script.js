02e9cd9778f05412bb60c69c471a5170f9f00646
Sergei Brezitskii <90458734+SergeiB2002@users.noreply.github.com>
2021-12-19 11:36:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function moveNavbar(){
+  let windowInnerWidth = window.innerWidth;
+  let navbar = $("#navbar");
+  if(windowInnerWidth <= 993) {
+      navbar.addClass("fixed-bottom");
+      navbar.addClass("bg-dark");
+      $(".video-bck").remove();
+  }
+  if(windowInnerWidth >= 993){
+      navbar.removeClass("fixed-bottom");
+      navbar.removeClass("bg-dark");
+  }
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
+function showDropdown(navbarDropdown, list){
+  $(navbarDropdown).addClass("show");
+  $(navbarDropdown).prop("aria-expanded", true);
+  $(list).addClass("show");
+}
+
+function hideDropdown(navbarDropdown, list){
+  let windowInnerWidth = window.innerWidth;
+  if(windowInnerWidth >= 993) {
+      $(navbarDropdown).removeClass("show");
+      $(navbarDropdown).prop("aria-expanded", false);
+      $(list).removeClass("show");
   }
 }
