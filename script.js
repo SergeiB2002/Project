ecabac10188a2fff6f98c053fcda9e241af199b1
Sergei Brezitskii <90458734+SergeiB2002@users.noreply.github.com>
2021-12-19 15:01:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,49 @@ function hideDropdown(navbarDropdown, list){
       $(list).removeClass("show");
   }
 }
+
+$(document).ready(function () {
+  $("#PartnersSlider1").slick({
+    adaptiveHeight: false,
+    mobileFirst: true,
+    dots: false,
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding:"20%",
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 5,
+            centerPadding:"10%",
+          }
+        },
+     ]
+}
+);
+$("#PartnersSlider2").slick({
+    adaptiveHeight: false,
+    mobileFirst: true,
+    dots: false,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding:"40%",
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 6,
+            centerPadding:"2%",
+          }
+        },
+     ]
+}); 
+});
