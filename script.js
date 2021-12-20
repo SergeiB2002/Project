e8c45575693cb92e4f16778cebb982c5456b87e1
Sergei Brezitskii <90458734+SergeiB2002@users.noreply.github.com>
2021-12-20 17:58:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -83,4 +83,45 @@ $(".PartnersSlider2").slick({
         },
      ]
 }); 
+  
+  $("#check").change(function () {
+  if ($("#check").is(":checked")) {
+      $("#submitButton").prop("disabled", false);
+  } else {
+      $("#submitButton").prop("disabled", true);
+  }
+});
+
+let data = document.querySelectorAll(".info");
+const ajaxSend = (formData) => {
+  fetch("https://formcarry.com/s/0tAvUSy9qZ1", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+  })
+      .then(function (response) {
+          alert("Сообщение отправлено");
+          data.forEach((element) => { element.value = ""; });
+          $("#check").prop("checked", false);
+          $("#submitButton").prop("disabled", true);
+          localStorage.clear();
+      })
+      .catch((error) => {alert(error);})
+};
+
+const forms = $("#Form");
+for (let i = 0; i < forms.length; i++) {
+  $("#submitButton").click(function (e) {
+      e.preventDefault();
+
+      let formData = new FormData(forms[i]);
+      formData = Object.fromEntries(formData);
+
+      ajaxSend(formData);
+  });
+}
+  
 });
